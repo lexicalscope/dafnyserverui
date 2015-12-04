ec80ebb934fb680ff8dd78e673c49c727eb95781
Tim Wood <lexicalscope@gmail.com>
2015-12-04 11:59:55 +0000

    get settings from GUI

--- src/main/java/com/lexicalscope/dafny/dafnyservergui/DafnyServerFrame.java
+++ src/main/java/com/lexicalscope/dafny/dafnyservergui/DafnyServerFrame.java
@@ -8,12 +8,17 @@ import java.awt.HeadlessException;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
@@ -30,27 +35,54 @@ public class DafnyServerFrame {
 
     public static void show(final DafnyServer server, final Arguments arguments) throws HeadlessException, InvocationTargetException, InterruptedException {
         SwingUtilities.invokeAndWait(() -> {
-            final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            final JFrame jFrame = new JFrame("Dafny");
-            jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            jFrame.setLayout(new BorderLayout());
-
-            final JPanel buttonPane = new JPanel(new GridBagLayout());
-            buttonPane.add(new JButton(new AbstractAction() {
-                @Override public void actionPerformed(final ActionEvent e) {
-                    server.sendMessage(message);
-                }
-            }));
-
-            final JTextPane dafnyOutputPane = new JTextPane();
-            jFrame.getContentPane().add(new JScrollPane(dafnyOutputPane), BorderLayout.CENTER);
-            jFrame.setSize(screenSize.width, 400);
-            jFrame.setVisible(true);
-
-            final DafnyServerFrame dafnyServerFrame = new DafnyServerFrame(dafnyOutputPane);
-
-            server.addOutputListener(line -> {SwingUtilities.invokeLater(() -> dafnyServerFrame.moreOutput(line));});
+            constructAndShowUi(server, arguments);
         });
+    }
+
+    static void constructAndShowUi(final DafnyServer server, final Arguments arguments) {
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final JFrame jFrame = new JFrame("Dafny");
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setLayout(new BorderLayout());
+
+        final JCheckBox traceTimesBox = new JCheckBox();
+        traceTimesBox.setSelected(true);
+        final JTextField fileField = new JTextField(40);
+        fileField.setText(arguments.file());
+
+        final JPanel buttonPane = new JPanel(new GridBagLayout());
+        buttonPane.add(new JButton(new AbstractAction() {
+            {
+                putValue(Action.NAME, "Verify!");
+            }
+            @Override public void actionPerformed(final ActionEvent e) {
+                final MessageToServer messageToServer = new MessageToServer();
+                messageToServer.source = fileField.getText();
+                messageToServer.filename = fileField.getText();
+                messageToServer.sourceIsFile = true;
+                final List<String> args = new ArrayList<>();
+                if(traceTimesBox.isSelected()) {
+                    args.add("/traceTimes");
+                }
+                messageToServer.args = args.toArray(new String[args.size()]);
+                server.sendMessage(messageToServer);
+            }
+        }));
+
+        final JPanel settingsPane = new JPanel(new GridBagLayout());
+        settingsPane.add(fileField);
+        settingsPane.add(traceTimesBox);
+
+        final JTextPane dafnyOutputPane = new JTextPane();
+        jFrame.getContentPane().add(buttonPane, BorderLayout.NORTH);
+        jFrame.getContentPane().add(new JScrollPane(dafnyOutputPane), BorderLayout.CENTER);
+        jFrame.getContentPane().add(settingsPane, BorderLayout.SOUTH);
+        jFrame.setSize(screenSize.width, 400);
+        jFrame.setVisible(true);
+
+        final DafnyServerFrame dafnyServerFrame = new DafnyServerFrame(dafnyOutputPane);
+
+        server.addOutputListener(line -> {SwingUtilities.invokeLater(() -> dafnyServerFrame.moreOutput(line));});
     }
 
     public void moreOutput(final String line)
--- src/main/java/com/lexicalscope/dafny/dafnyservergui/DafnyServerGui.java
+++ src/main/java/com/lexicalscope/dafny/dafnyservergui/DafnyServerGui.java
@@ -14,37 +14,23 @@ public class DafnyServerGui
     {
         final Arguments arguments = CliFactory.parseArguments(Arguments.class, args);
 
-        final MessageToServer messageToServer = new MessageToServer();
-        messageToServer.source = arguments.file();
-        messageToServer.filename = arguments.file();
-        messageToServer.sourceIsFile = true;
-        messageToServer.args = new String[]{};//"/traceTimes"};
+
 
         final ProcessBuilder pb = new ProcessBuilder(arguments.server());
         pb.redirectErrorStream(true);
         final ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(1);
         final Process process = pb.start();
 
-
         final DafnyServer server = new DafnyServer(newFixedThreadPool, process);
-        server.addOutputListener(message -> {System.out.println(message);});
-
-        DafnyServerFrame.show(server);
-            server.sendMessage(messageToServer);
-
-            server.pipeOutput();
-
-            Thread.sleep(15000);
-
-            server.sendMessage(messageToServer);
-
-            Thread.sleep(5000);
-
-
         Runtime.getRuntime().addShutdownHook(new Thread(){
             @Override public void run() {
                 server.close();
             }
         });
+
+        server.addOutputListener(message -> {System.out.println(message);});
+        DafnyServerFrame.show(server, arguments);
+
+        server.pipeOutput();
     }
 }
